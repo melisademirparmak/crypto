f890c24ef6d8bf54a52b41e0a16f77ecae2de67b
melisademirparmak <melisademirparmaak@gmail.com>
2021-06-10 13:40:34 +0300

    navbar done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./css/style.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <div className="logo">
+          <i class="fas fa-angle-double-left"></i>coinci
+          <i class="fas fa-angle-double-right"></i>
+        </div>
+        <form >
+          <input type="text" class="serachBar" placeholder="Search"/>
+        </form>
+      </div>
+    </>
   );
 }
 

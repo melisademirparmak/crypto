b1b0112b10cc5c84cc1b16ec0fb6f7af8bf03248
melisademirparmak <melisademirparmaak@gmail.com>
2021-06-10 18:23:35 +0300

    design edited

--- src/App.js
+++ src/App.js
@@ -52,8 +52,6 @@ function App() {
               name={coin.name}
               price={coin.current_price}
               symbol={coin.symbol}
-              marketcap={coin.market_cap}
-              volume={coin.total_volume}
               image={coin.image}
               priceChange={coin.price_change_percentage_24h}
             />
--- src/components/Coin.js
+++ src/components/Coin.js
@@ -5,32 +5,34 @@ const Coin = ({
   name,
   price,
   symbol,
-  marketcap,
-  volume,
+
   image,
   priceChange,
 }) => {
   return (
-    <div className="container">
-      <div className="cryptoCoin">
+    <div className="cryptoCoin">
+      <div className="img">
         <img src={image} alt={`${name}`} className="CoinLogo" />
-        <div className="coinNameWrap"></div>
-        <h1 className="coinName">{name}</h1>
-        <p className="coinSymbol">{symbol}</p>
-        <p className="coinPrice">${price.toLocaleString()}</p>
-        <p className="coinMarketcap">${marketcap.toLocaleString()}</p>
-        <p className="coinVolume">Volume (24H) : ${volume.toLocaleString()}</p>
-        {priceChange < 0 ? (
-          <div className="priceContainerDOWN">
-            <i className="fas fa-angle-down fa-2x"></i>
-            <p className="priceChange">{priceChange.toFixed(2)}%</p>
-          </div>
-        ) : (
-          <div className="priceContainerUP">
-            <i className="fas fa-angle-up fa-2x"></i>
-            <p className="priceChange">{priceChange.toFixed(2)}%</p>
-          </div>
-        )}
+      </div>
+      <div className="coinNameWrap">
+        <div className="b-left">
+          <h1 className="coinName">{name}</h1>
+          <p className="coinSymbol">{symbol}</p>
+          <p className="coinPrice">${price.toLocaleString()}</p>
+        </div>
+        <div>
+          {priceChange < 0 ? (
+            <div className="priceContainerDown flex-direction">
+              <p className="priceChange">{priceChange.toFixed(2)}%</p>
+              <i className="fas fa-angle-down fa-2x"></i>
+            </div>
+          ) : (
+            <div className="priceContainerUP flex-direction">
+              <i className="fas fa-angle-up fa-2x"></i>
+              <p className="priceChange">{priceChange.toFixed(2)}%</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
